69a8ec1b0684990e0817b727ef878c83abbaa1b2
Cheiron Zhang <cheironde@gmail.com>
2016-04-07 11:27:05 +0800

    整理文件

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	sourcemaps = require('gulp-sourcemaps'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	jshint = require('gulp-jshint'),
+	uglify = require('gulp-uglify'),
+	htmlmin = require('gulp-htmlmin'),
+	rename = require('gulp-rename'),
+	livereload = require('gulp-livereload'),
+	plumber = require('gulp-plumber'),
+	notify = require('gulp-notify');
+
+// Configs
+var htmlminConfig = {
+	removeComments: true,
+	collapseWhitespace: true,
+	collapseBooleanAttributes: true,
+	removeEmptyAttributes: true,
+	removeScriptTypeAttributes: true,
+	removeStyleLinkTypeAttributes: true,
+	minifyJS: true,
+	minifyCSS: true
+};
+var autoprefixerConfig = {
+	browsers: ['last 2 versions', 'Android >= 4.0'],
+	cascade: true,
+	remove: true
+}
+
+gulp.task('html', function(){
+	return gulp.src('src/*.html')
+	.pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+	.pipe(htmlmin(htmlminConfig))
+	.pipe(gulp.dest('dist'));
+});
+
+gulp.task('style', function(){
+	return gulp.src('src/less/*.less')
+	.pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+	.pipe(sourcemaps.init())
+	.pipe(less())
+	.pipe(sourcemaps.write())
+	.pipe(autoprefixer(autoprefixerConfig))
+	.pipe(gulp.dest('src/css'))
+	.pipe(rename({ suffix: '.min'}))
+	.pipe(minifycss())
+	.pipe(gulp.dest('src/css'))
+	.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('js', function(){
+	return gulp.src(['src/js/*.js', '!src/js/*.min.js'])
+	.pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'))
+	.pipe(rename({ suffix: '.min'}))
+	.pipe(uglify())
+	.pipe(gulp.dest('src/js'))
+	.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('src/*.html', ['html']);
+	gulp.watch('src/less/*.less', ['style']);
+	gulp.watch(['src/js/*.js', '!src/js/*.min.js'], ['js']);
+	livereload.listen();
+	gulp.watch(['dist/*']).on('change', livereload.changed);
+});
+	
+gulp.task('default', function(){
+	gulp.start('html', 'style', 'js', 'watch');
+});
