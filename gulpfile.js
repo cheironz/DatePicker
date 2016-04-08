2dc6cb107040209b8b8266aaa8ba07baf6860298
Cheiron Zhang <cheironde@gmail.com>
2016-04-08 10:31:01 +0800

    修改gulpfile，删除已生成文件

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@ var gulp = require('gulp'),
 	sourcemaps = require('gulp-sourcemaps'),
 	autoprefixer = require('gulp-autoprefixer'),
 	minifycss = require('gulp-minify-css'),
+	concat = require('gulp-concat'),
 	jshint = require('gulp-jshint'),
 	uglify = require('gulp-uglify'),
+	cache = require('gulp-cache'),
+	imagemin = require('gulp-imagemin'),
 	htmlmin = require('gulp-htmlmin'),
 	rename = require('gulp-rename'),
 	livereload = require('gulp-livereload'),
@@ -12,6 +15,8 @@ var gulp = require('gulp'),
 	notify = require('gulp-notify');
 
 // Configs
+var combineCSSName = 'all.css';
+var combineJSName = 'all.js';
 var htmlminConfig = {
 	removeComments: true,
 	collapseWhitespace: true,
@@ -40,34 +45,51 @@ gulp.task('style', function(){
 	.pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
 	.pipe(sourcemaps.init())
 	.pipe(less())
-	.pipe(sourcemaps.write())
 	.pipe(autoprefixer(autoprefixerConfig))
 	.pipe(gulp.dest('src/css'))
+	.pipe(gulp.dest('dist/css'))
+	.pipe(concat(combineCSSName))
+	.pipe(gulp.dest('src/css'))
+	.pipe(gulp.dest('dist/css'))
 	.pipe(rename({ suffix: '.min'}))
 	.pipe(minifycss())
+	.pipe(sourcemaps.write('./'))
 	.pipe(gulp.dest('src/css'))
 	.pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('js', function(){
-	return gulp.src(['src/js/*.js', '!src/js/*.min.js'])
+	return gulp.src(['src/js/*.js', '!src/js/' + combineJSName, '!src/js/*.min.js'])
 	.pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+	.pipe(sourcemaps.init())
 	.pipe(jshint())
 	.pipe(jshint.reporter('default'))
+	.pipe(concat(combineJSName))
+	.pipe(gulp.dest('src/js'))
+	.pipe(gulp.dest('dist/js'))
 	.pipe(rename({ suffix: '.min'}))
 	.pipe(uglify())
+	.pipe(sourcemaps.write('./'))
 	.pipe(gulp.dest('src/js'))
 	.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('image', function(){
+	return gulp.src('src/images/*')
+	.pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+	.pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+	.pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('watch', function(){
 	gulp.watch('src/*.html', ['html']);
 	gulp.watch('src/less/*.less', ['style']);
-	gulp.watch(['src/js/*.js', '!src/js/*.min.js'], ['js']);
+	gulp.watch(['src/js/*.js', '!src/js/' + combineJSName, '!src/js/*.min.js'], ['js']);
+	gulp.watch('src/images/*', ['image'])
 	livereload.listen();
-	gulp.watch(['dist/*/*']).on('change', livereload.changed);
+	gulp.watch(['dist/*']).on('change', livereload.changed);
 });
 	
 gulp.task('default', function(){
-	gulp.start('html', 'style', 'js', 'watch');
+	gulp.start('html', 'style', 'js', 'image', 'watch');
 });
